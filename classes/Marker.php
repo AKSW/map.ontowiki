3a4dda2489581a1d9d4721e99bd32f67910b0d82
Natanael Arndt <arndtn@gmail.com>
2011-11-22 00:08:36 +0100

    coding standarts

--- classes/Marker.php
+++ classes/Marker.php
@@ -1,18 +1,18 @@
 <?php
-// vim: sw=4:sts=4:expandtab
+
 /**
  * Marker-Class of the OW MapPlugin
  *
  * a Marker is an object representing an ontology object. This marker object
  * will later be shown on a map.
  *
- * @category   OntoWiki
- * @package    OntoWiki_extensions_components_map
+ * @category OntoWiki
+ * @package OntoWiki_extensions_components_map
+ * @author Natanael Arndt <arndtn@gmail.com>
  * @author OW MapPlugin-Team <mashup@comiles.eu>
- * @version 1.0.0
- * @package MapPlugin
  */
-class Marker {
+class Marker
+{
 
     /**
      * The geo location (normaly on the earth)
@@ -25,10 +25,11 @@ class Marker {
     public $uri;
 
     /**
-     *  status properties indeicating, if a marker is on the Map (visibility) and if a marker is clustered and represented by a cluster on the map
+     * status properties indeicating, if a marker is on the Map (visibility) and if a marker is clustered and
+     * represented by a cluster on the map
      */
-    private $onMap, $inCluster;
-    
+    private $_onMap, $_inCluster;
+
     //public $isCluster;
 
     /**
@@ -40,7 +41,8 @@ class Marker {
      * Constructor of a Marker object.
      * @param $uri uri of the resource, which is represented by the marker
      */
-    public function __construct( $uri ) {
+    public function __construct($uri)
+    {
         $this->uri = $uri;
     }
 
@@ -48,52 +50,58 @@ class Marker {
      * Gets the uri of the object, which is represented by the marker.
      * @return uri of the object, which is represented by the marker
      */
-    public function getUri() {
+    public function getUri()
+    {
         return $this->uri;
     }
 
     /**
-     * Sets the onMap-attribute, which indicates whether the marker is already 
+     * Sets the _onMap-attribute, which indicates whether the marker is already 
      * added to the map.
      * @param $onMapIn indicates whether the marker is already added to the map
      */
-    public function setOnMap( $onMapIn ) {
-        $this->onMap = $onMapIn;
+    public function setOnMap($onMapIn)
+    {
+        $this->_onMap = $onMapIn;
     }
 
     /**
-     * Gets the onMap-attribute, which indicates whether the marker is already 
+     * Gets the _onMap-attribute, which indicates whether the marker is already 
      * added to the map.
      * @return marker is already added to the map
      */
-    public function getOnMap( ) {
-        return $this->onMap;
+    public function getOnMap()
+    {
+        return $this->_onMap;
     }
 
     /**
-     * Sets the inCluster-attribute, which indicates whether the marker is 
+     * Sets the _inCluster-attribute, which indicates whether the marker is 
      * contained in a cluster.
      * @param $inClusterIn indicates whether the marker is contained in a 
      * cluster
      */
-    public function setInCluster( $inClusterIn ) {
-        $this->inCluster = $inClusterIn;
+    public function setInCluster($inClusterIn)
+    {
+        $this->_inCluster = $inClusterIn;
     }
 
     /**
-     * Gets the inCluster-attribute, which indicates whether the marker is 
+     * Gets the _inCluster-attribute, which indicates whether the marker is 
      * contained in a cluster.
      * @return marker is contained in a cluster
      */
-    public function getInCluster( ) {
-        return $this->inCluster;
+    public function getInCluster()
+    {
+        return $this->_inCluster;
     }
 
     /**
      * Sets the longitude of the marker.
      * @param $lon longitude of the marker
      */
-    public function setLon( $lon ) {
+    public function setLon($lon)
+    {
         $this->longitude = $lon;
     }
 
@@ -101,7 +109,8 @@ class Marker {
      * Sets the latitude of the marker.
      * @param $lon latitude of the marker
      */
-    public function setLat( $lat ) {
+    public function setLat($lat)
+    {
         $this->latitude = $lat;
     }
 
@@ -109,7 +118,8 @@ class Marker {
      * Gets the longitude of the marker.
      * @return longitude of the marker
      */
-    public function getLon( ) {
+    public function getLon()
+    {
         return $this->longitude;
     }
 
@@ -117,7 +127,8 @@ class Marker {
      * Gets the latitude of the marker.
      * @return latitude of the marker
      */
-    public function getLat( ) {
+    public function getLat()
+    {
         return $this->latitude;
     }
 
@@ -125,7 +136,8 @@ class Marker {
      * Sets the icon of the marker.
      * @param $icon of the marker
      */
-    public function setIcon( $icon ) {
+    public function setIcon($icon)
+    {
         $this->icon = $icon;
     }
 
@@ -133,9 +145,9 @@ class Marker {
      * Gets the icon of the marker.
      * @return icon of the marker
      */
-    public function getIcon( ) {
+    public function getIcon()
+    {
         return $this->icon;
     }
 
 }
-?>

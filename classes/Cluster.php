6cab29b539068a4ed2b420d6361513b420f66c6f
Natanael Arndt <arndtn@gmail.com>
2011-11-22 00:22:48 +0100

    coding standarts

--- classes/Cluster.php
+++ classes/Cluster.php
@@ -1,25 +1,24 @@
 <?php
-// vim: sw=4:sts=4:expandtab
 
 require_once $this->_componentRoot.'classes/Marker.php';
 
 /**
  * Cluster-Class of the OW MapPlugin
  *
- * @category   OntoWiki
- * @package    OntoWiki_extensions_components_map
+ * @category OntoWiki
+ * @package OntoWiki_extensions_components_map
+ * @author Natanael Arndt <arndtn@gmail.com>
  * @author OW MapPlugin-Team <mashup@comiles.eu>
- * @version 1.0.0
- * @package MapPlugin
  */
-class Cluster extends Marker {
+class Cluster extends Marker
+{
 
     /**
      * set isCluster on true, to identify this object as cluster in the json serialization
      * -- not implemented in javascript --
      */
     public $isCluster = true;
-    private $isMicroCluster;
+    private $_isMicroCluster;
 
     /**
      * The array of the markers in this cluster
@@ -29,33 +28,35 @@ class Cluster extends Marker {
     /**
      * The area of the containing markers
      */
-    private $area = array();
+    private $_area = array();
 
     /**
      * A boolean telling, if the cell of this cluster contains the dateline
      */
-    private $withDateLine;
+    private $_withDateLine;
 
     /**
      * Set the inherited inCluster false, because a cluster can't be in a cluster
      */
-    private static $inCluster = false;
+    private $_inCluster = false;
 
     /**
      * Constructor of a Cluster object.
      */
-    public function __construct($area) {
-        parent::__construct( null );
-        $this->area = $area;
+    public function __construct($area)
+    {
+        parent::__construct(null);
+        $this->_area = $area;
     }
 
     /**
      * Destructor of a Cluster object. Sets the inCluster-attribute of the 
      * contained markers to false.
      */
-    public function __destruct( ) {
-        for($i = 0; $i < count( $this->containingMarkers ); $i++ ) {
-            $this->containingMarkers[$i]->setInCluster( false );
+    public function __destruct()
+    {
+        for ($i = 0; $i < count($this->containingMarkers); $i++) {
+            $this->containingMarkers[$i]->setInCluster(false);
         }
     }
 
@@ -63,8 +64,9 @@ class Cluster extends Marker {
      * Adds a marker to the cluster.
      * @param &$marker a reference of a Marker object
      */
-    public function addMarker( &$marker ) {
-    	$marker->setInCluster(true);
+    public function addMarker(&$marker)
+    {
+        $marker->setInCluster(true);
         $this->containingMarkers[] = &$marker;
     }
 
@@ -72,37 +74,39 @@ class Cluster extends Marker {
      * Returns the count of the markers in the cluster.
      * @return an integer
      */
-    public function countMarkers( ) {
-        return count( $this->containingMarkers );
+    public function countMarkers()
+    {
+        return count($this->containingMarkers);
     }
 
     /**
      * Calculates latitude and longitude for the cluster (arithmetic mean).
      */
-    public function createLonLat( $overwrite = true ) {
+    public function createLonLat($overwrite = true)
+    {
 
         /**
          * Check if the longitude and latitude is already set.
          */
-    	if($this->isMicroCluster) {
-    		$this->longitude = $this->containingMarkers[0]->getLon( );
-    		$this->latitude = $this->containingMarkers[0]->getLat( );
-    	} else if($overwrite || !isset($this->longitude) OR !isset($this->latitude)){
+        if ($this->_isMicroCluster) {
+            $this->longitude = $this->containingMarkers[0]->getLon();
+            $this->latitude = $this->containingMarkers[0]->getLat();
+        } else if ($overwrite || !isset($this->longitude) OR !isset($this->latitude)) {
 
             /**
              * Adding up the latitude respactively longitude of all markers contained by the cluster
              */
             $lonTmp = 0;
             $latTmp = 0;
-            for($i = 0; $i < count($this->containingMarkers); $i++){
-                $lonTmp += $this->containingMarkers[$i]->getLon( );
-                $latTmp += $this->containingMarkers[$i]->getLat( );
+            for ($i = 0; $i < count($this->containingMarkers); $i++) {
+                $lonTmp += $this->containingMarkers[$i]->getLon();
+                $latTmp += $this->containingMarkers[$i]->getLat();
             }
 
             /**
              * Divide by the amount of the contained markers to calcule the arithmetic mean
              */
-            if(count($this->containingMarkers) > 0){
+            if (count($this->containingMarkers) > 0) {
                 $lonTmp /= count($this->containingMarkers);
                 $latTmp /= count($this->containingMarkers);
             }
@@ -110,15 +114,14 @@ class Cluster extends Marker {
             /**
              * Check if the cluster contains markers from the right and from the left side of the dateline
              */
-            if($this->withDateLine) {
+            if ($this->_withDateLine) {
 
                 /**
                  * If the dateline is in the cluster turn the lon half around the world
                  */
-                if($lonTmp > 0) {
+                if ($lonTmp > 0) {
                     $lonTmp -= 180;
-                }
-                else {
+                } else {
                     $lonTmp += 180;
                 }
             }
@@ -135,35 +138,40 @@ class Cluster extends Marker {
      * Get the Markers in the Cluster.
      * @return an array of marker objects
      */
-    public function getContent( ) {
+    public function getContent()
+    {
         return $this->containingMarkers;
     }
-    
-    public function getLat() {
-    	createLonLat(true);
-    	return parent::getLat();
-    }
-    
-	public function getLon() {
-    	createLonLat(true);
-    	return parent::getLon();
+
+    public function getLat()
+    {
+        createLonLat(true);
+        return parent::getLat();
     }
 
-    public function setUri($uri) {
-    	$this->uri = $uri;
+    public function getLon()
+    {
+        createLonLat(true);
+        return parent::getLon();
     }
-    
-    public function setIsMicroCluster($isMicroCluster) {
-    	$this->isMicroCluster = $isMicroCluster;
+
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
     }
-    
+
+    public function setIsMicroCluster($isMicroCluster)
+    {
+        $this->_isMicroCluster = $isMicroCluster;
+    }
+
     /**
      * Set a value if the dateline is in the cluster or not
      * @param $var boolean
      */
-    public function setWithDateLine( $var ) {
-        $this->withDateLine = $var;
+    public function setWithDateLine( $var )
+    {
+        $this->_withDateLine = $var;
     }
 
 }
-?>
